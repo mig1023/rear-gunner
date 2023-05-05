b3a18a533f7bba725fab722ff89db8c6a0231882
mig1023 <mig1023@mail.ru>
2023-05-05 17:16:34 +0300

    shooting

--- Assets/Scenes/Scripts/TurretRotate.cs
+++ Assets/Scenes/Scripts/TurretRotate.cs
@@ -6,10 +6,13 @@ public class TurretRotate : MonoBehaviour
 {
 	public Transform BoneTurret;   
 	public Transform BoneBarrelTurn;
+	public GameObject shell;
 	
 	public int verticalLimithMin = -90;
 	public int verticalLimithMax = -15;
 	public float speed = 3;
+	public float shellSize = 0.45f;
+	public float shellSpeed = 700f;
 	
 	Vector3 rotateY = Vector2.zero;
 
@@ -23,5 +26,21 @@ public class TurretRotate : MonoBehaviour
 		rotateY.x += Input.GetAxis("Mouse Y") * speed;
 		rotateY.x = Mathf.Clamp(rotateY.x, verticalLimithMin, verticalLimithMax);
 		BoneBarrelTurn.rotation = Quaternion.Euler(rotateY);
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			Transform barrel = BoneBarrelTurn.GetChild(0);
+			
+			Quaternion shellRot = Quaternion.Euler(
+				barrel.eulerAngles.x+Random.Range(-3f,3f),
+				barrel.eulerAngles.y+Random.Range(-3f,3f),
+				barrel.eulerAngles.z+Random.Range(-3f,3f));
+			
+			var instShell = Instantiate(shell, barrel.position, shellRot) ; 
+			instShell.transform.localScale = new Vector3(shellSize, shellSize, shellSize);
+			float shot = shellSpeed + Random.Range(-1 * shellSpeed * 0.2f, shellSpeed * 0.2f);
+			Vector3 dir = shellRot * Vector3.up * shot;
+			instShell.GetComponent<Rigidbody>().AddForce(dir);
+		}
 	}
 }
