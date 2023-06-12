10dd424ac4124bb007ed7eac7c58c75657e31b7d
mig1023 <mig1023@mail.ru>
2023-06-12 12:24:37 +0300

    Shell explosion

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+	public ParticleSystem particle_Explosive;
+	
 	void Start()
 	{
 		StartCoroutine(Selfliquidator());
 	}
-
-	void Update()
-	{
 	
+	void OnTriggerEnter(Collider col)
+	{
+		Quaternion explosion = Quaternion.Euler(Random.Range(0, 360),Random.Range(0, 360),Random.Range(0,360));
+		var instExplosion = Instantiate(particle_Explosive, transform.position, explosion); 
+		instExplosion.transform.localScale = new Vector3(3, 3, 3);
+
+		Destroy(instExplosion.gameObject, particle_Explosive.main.duration); 
+		Destroy(gameObject);
 	}
 	
 	private IEnumerator Selfliquidator()
