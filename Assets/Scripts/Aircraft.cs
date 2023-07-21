da7db3ab479c04f1f1a18e0cc861cdd39434f706
mig1023 <mig1023@mail.ru>
2023-07-21 12:11:56 +0300

    destroy fix

--- Assets/Scripts/Aircraft.cs
+++ Assets/Scripts/Aircraft.cs
@@ -11,21 +11,32 @@ public class Aircraft : MonoBehaviour
 	private float Falling = 0f;
 	private bool Dead = false;
 
-    private void FixedUpdate()
+	private void FixedUpdate()
 	{
 		Vector3 movement = transform.forward * Speed * Time.deltaTime;
 
-        if (Dead)
-        {
-            Falling -= 0.001f;
+		if (Dead)
+		{
+			Falling -= 0.001f;
 
-            Vector3 direction = movement;
-            direction.y = Falling;
-            transform.rotation = Quaternion.LookRotation(direction);
-        }
+			Vector3 direction = movement;
+			direction.y = Falling;
+			transform.rotation = Quaternion.LookRotation(direction);
+		}
 
-        Rigidbody body = GetComponent<Rigidbody>();
+		Rigidbody body = GetComponent<Rigidbody>();
 		body.MovePosition(body.position + movement);
+
+		if ((ParticleExplosive != null) && (transform.position.y <= 5))
+		{
+			var instParticle = Instantiate(ParticleExplosive, transform.position,
+				Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+
+			instParticle.transform.localScale = new Vector3(15, 15, 5);
+
+			Destroy(instParticle.gameObject, ParticleExplosive.main.duration);
+			Destroy(gameObject);
+		}
 	}
 
 	public void OnTriggerEnter(Collider collision)
