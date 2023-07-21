06e8cbec5bc459de33275ab6d5ee6067a1d5cdbc
mig1023 <mig1023@mail.ru>
2023-07-21 12:16:14 +0300

    ltl refactoring

--- Assets/Scripts/Aircraft.cs
+++ Assets/Scripts/Aircraft.cs
@@ -16,27 +16,38 @@ public class Aircraft : MonoBehaviour
 		Vector3 movement = transform.forward * Speed * Time.deltaTime;
 
 		if (Dead)
-		{
-			Falling -= 0.001f;
+			DeadFalling(movement);
 
-			Vector3 direction = movement;
-			direction.y = Falling;
-			transform.rotation = Quaternion.LookRotation(direction);
-		}
-
-		Rigidbody body = GetComponent<Rigidbody>();
-		body.MovePosition(body.position + movement);
+		Move(movement);
 
 		if ((ParticleExplosive != null) && (transform.position.y <= 5))
-		{
-			var instParticle = Instantiate(ParticleExplosive, transform.position,
-				Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+			Destruction();
+	}
 
-			instParticle.transform.localScale = new Vector3(15, 15, 5);
+	private void DeadFalling(Vector3 movement)
+    {
+		Falling -= 0.001f;
 
-			Destroy(instParticle.gameObject, ParticleExplosive.main.duration);
-			Destroy(gameObject);
-		}
+		Vector3 direction = movement;
+		direction.y = Falling;
+		transform.rotation = Quaternion.LookRotation(direction);
+	}
+
+	private void Move(Vector3 movement)
+    {
+		Rigidbody body = GetComponent<Rigidbody>();
+		body.MovePosition(body.position + movement);
+	}
+
+	private void Destruction()
+    {
+		var instParticle = Instantiate(ParticleExplosive, transform.position,
+			Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+
+		instParticle.transform.localScale = new Vector3(15, 15, 5);
+
+		Destroy(instParticle.gameObject, ParticleExplosive.main.duration);
+		Destroy(gameObject);
 	}
 
 	public void OnTriggerEnter(Collider collision)
