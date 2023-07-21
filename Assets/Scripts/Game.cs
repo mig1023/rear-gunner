edde370627a0705102874084417df5036df041ea
mig1023 <mig1023@mail.ru>
2023-07-21 13:07:37 +0300

    random aircraft start

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public GameObject Aircraft;
+    public GameObject[] Aircraft;
 
     private float Behind = 300;
     private float Altitude = 30;
@@ -23,6 +23,7 @@ public class Game : MonoBehaviour
         shipPosition.y += Altitude;
         shipPosition.x += Random.Range(-30, 30);
 
-        Instantiate(Aircraft, shipPosition, battleship.gameObject.transform.rotation);
+        int typeAircraft = Random.Range(0, Aircraft.Length);
+        Instantiate(Aircraft[typeAircraft], shipPosition, battleship.gameObject.transform.rotation);
     }
 }
